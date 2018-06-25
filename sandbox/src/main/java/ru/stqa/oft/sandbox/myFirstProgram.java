5366e05a9a2fdc0c509fc1cf10b6c9205cf88a45
Bartosz Kasperowicz <Bartek@MacBook-Pro-Bartosz.local>
2018-06-25 22:22:29 +0200

    My first program

--- sandbox/src/main/java/ru/stqa/oft/sandbox/myFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/oft/sandbox/myFirstProgram.java
@@ -1,4 +1,6 @@
-public class MyFirstProgram {
+package ru.stqa.oft.sandbox;
+
+public class myFirstProgram {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
