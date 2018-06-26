e1da7f8253c40be254f0025bb03c85829f3c9ff9
Bartosz Kasperowicz <Bartek@MacBook-Pro-Bartosz.local>
2018-06-26 19:46:44 +0200

    added few function

--- sandbox/src/main/java/ru/stqa/oft/sandbox/myFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/oft/sandbox/myFirstProgram.java
@@ -4,7 +4,27 @@ public class myFirstProgram {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+        hello("world");
+        hello("user");
+        hello("Bartosz");
+
+        double l = 5;
+        System.out.println("pole kwadratu o boku "+ l +" = " + area(l));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("pole prostokatu o boku "+ a + " i " + b +" = " + area(a, b));
+
+    }
+    public static void hello(String somebody)  {
+        System.out.println("Hello, " + somebody +"!");
     }
 
+    public static double area(double len){
+        return len * len;
+    }
+
+    public static double area(double a, double b){
+    return a * b;
+    }
 }
