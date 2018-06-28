93bb10fab50885a1bcbaf1f5459f239b5b4df2a3
Bartosz Kasperowicz <Bartek@MacBook-Pro-Bartosz.local>
2018-06-28 20:35:04 +0200

    Added task two

---
+++ sandbox/src/main/java/ru/stqa/oft/sandbox/Point.java
@@ -0,0 +1,13 @@
+package ru.stqa.oft.sandbox;
+
+public class Point {
+
+  public double x;
+  public double y;
+
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  }
--- sandbox/src/main/java/ru/stqa/oft/sandbox/myFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/oft/sandbox/myFirstProgram.java
@@ -4,27 +4,40 @@ public class myFirstProgram {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        hello("world");
-        hello("user");
-        hello("Bartosz");
+  //      hello("world");
+  //      hello("user");
+  //      hello("Bartosz");
 
-        double l = 5;
-        System.out.println("pole kwadratu o boku "+ l +" = " + area(l));
+  //      double l = 5;
+  //      System.out.println("pole kwadratu o boku "+ l +" = " + area(l));
 
-        double a = 4;
-        double b = 6;
-        System.out.println("pole prostokatu o boku "+ a + " i " + b +" = " + area(a, b));
+  //      double a = 4;
+  //      double b = 6;
+  //      System.out.println("pole prostokatu o boku "+ a + " i " + b +" = " + area(a, b));
 
+
+        Point p1 = new Point(1, 1);
+        Point p2 = new Point(2, 2);
+        System.out.println("Distance between points: +" + distance(p1, p2));
     }
-    public static void hello(String somebody)  {
-        System.out.println("Hello, " + somebody +"!");
-    }
+   // public static void hello(String somebody)  {
+   //     System.out.println("Hello, " + somebody +"!");
+   // }
 
-    public static double area(double len){
-        return len * len;
-    }
+   // public static double area(double len){
+   //    return len * len;
+   // }
 
-    public static double area(double a, double b){
-    return a * b;
+    //public static double area(double a, double b){
+    //return a * b;
+    //}
+    public static double distance(Point p1, Point p2){
+        double dx;
+        double dy;
+        dx = p2.x - p1.x;
+        dy = p2.y - p1.y;
+
+        return Math.sqrt((dx * dx) + (dy * dy));
+
     }
 }
