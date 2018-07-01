237182c394a34e6dc108c909a01047e413dabd0e
Bartosz Kasperowicz <Bartek@MacBook-Pro-Bartosz.local>
2018-07-01 14:07:21 +0200

    Add

--- sandbox/src/main/java/ru/stqa/oft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/oft/sandbox/Point.java
@@ -2,6 +2,7 @@ package ru.stqa.oft.sandbox;
 
 public class Point {
 
+
   public double x;
   public double y;
 
@@ -9,5 +10,13 @@ public class Point {
     this.x = x;
     this.y = y;
   }
+  public double area(Point p1, Point p2){ //P
+    double dx;
+    double dy;
+    dx = p1.x - p2.x;
+    dy = p1.y - p2.y;
 
+    return Math.sqrt((dx * dx) + (dy * dy));
+
+  }
   }
--- sandbox/src/main/java/ru/stqa/oft/sandbox/myFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/oft/sandbox/myFirstProgram.java
@@ -18,15 +18,22 @@ public class myFirstProgram {
   //      System.out.println("pole prostokatu o boku "+ a + " i " + b +" = " + area(a, b));
 
 
-        Point p1 = new Point(1, 1);
-        Point p2 = new Point(2, 2);
+
        // System.out.println("Distance between points: +" + distance(p1, p2));
 
-        double distance = distance(p1, p2);
+        //double distance = distance(p1, p2);
 
-        boolean weryfikacja = verify(p1, p2, distance);
-        System.out.println("Verification status (true / false):" + weryfikacja);
-        System.out.println("Distance between points:" + distance);
+        //boolean weryfikacja = verify(p1, p2, distance);
+       // System.out.println("Verification status (true / false):" + weryfikacja);
+
+      Point p1 = new Point(1, 1);
+      Point p2 = new Point(6, 9);
+
+
+      //Point p = new Point((p1.x, p1.y)(p2.x)(p2.y));
+
+
+        System.out.println("Distance between points:" + p1.area(p1,p2));
 
 
 
@@ -42,32 +49,24 @@ public class myFirstProgram {
     //public static double area(double a, double b){
     //return a * b;
     //}
-    public static double distance(Point p1, Point p2){
-        double dx;
-        double dy;
-        dx = p2.x - p1.x;
-        dy = p2.y - p1.y;
 
-        return Math.sqrt((dx * dx) + (dy * dy));
-
-    }
-    public static boolean verify(Point p1, Point p2, double wynikDistance){
-        double dx;
-        double dy;
-        dx = p2.x - p1.x;
-        dy = p2.y - p1.y;
-
-        double wynik;
-        wynik = pow(dx, 2) + pow(dy,2);
-
-
-        if((int) wynik == (int) pow(wynikDistance,2) ){
-            return true;
-
-        }
-        else{
-            return false;
-        }
-
-    }
+//    public static boolean verify(Point p1, Point p2, double wynikDistance){
+//        double dx;
+//        double dy;
+//        dx = p2.x - p1.x;
+//        dy = p2.y - p1.y;
+//
+//        double wynik;
+//        wynik = pow(dx, 2) + pow(dy,2);
+//
+//
+//        if((int) wynik == (int) pow(wynikDistance,2) ){
+//            return true;
+//
+//        }
+//        else{
+//            return false;
+//        }
+//
+//    }
 }
