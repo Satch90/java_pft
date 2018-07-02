892e81352bf0059065fc277e7b80951a22c33505
Bartosz Kasperowicz <Bartek@MacBook-Pro-Bartosz.local>
2018-07-02 18:13:33 +0200

    Split @test fixture

--- addressbook-web-test/src/test/java/ru/stqa/oft/addressbook/GroupCreationTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/oft/addressbook/GroupCreationTests.java
@@ -1,30 +1,24 @@
 package ru.stqa.oft.addressbook;
 
-import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import static org.testng.Assert.*;
 
 import java.util.concurrent.TimeUnit;
-import java.util.Date;
-import java.io.File;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
-import static org.openqa.selenium.OutputType.*;
 
 public class GroupCreationTests {
-    FirefoxDriver wd;
+//    FirefoxDriver wd;
+    ChromeDriver wd;
+
     
     @BeforeMethod
     public void setUp() throws Exception {
-        wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+//        wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+      System.setProperty("webdriver.chrome.driver", "/Applications/chromedriver");
+        wd = new ChromeDriver();
         wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    }
-    
-    @Test
-    public void GroupCreationTests() {
         wd.get("http://localhost/addressbook/group.php");
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
@@ -34,6 +28,12 @@ public class GroupCreationTests {
         wd.findElement(By.name("pass")).clear();
         wd.findElement(By.name("pass")).sendKeys("secret");
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+
+    }
+    
+    @Test
+    public void testGroupCreation() {
+
         wd.findElement(By.linkText("groups")).click();
         wd.findElement(By.name("new")).click();
         wd.findElement(By.name("group_name")).click();
@@ -49,13 +49,13 @@ public class GroupCreationTests {
         wd.findElement(By.name("submit")).click();
         wd.findElement(By.linkText("groups")).click();
     }
-    
+
     @AfterMethod
     public void tearDown() {
         wd.quit();
     }
-    
-    public static boolean isAlertPresent(FirefoxDriver wd) {
+
+    public static boolean isAlertPresent(ChromeDriver wd) {
         try {
             wd.switchTo().alert();
             return true;
@@ -63,4 +63,6 @@ public class GroupCreationTests {
             return false;
         }
     }
+    
+
 }
