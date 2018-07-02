f4014eec1fd868fc3bd3c13eb6b0df00b120ec64
Bartosz Kasperowicz <Bartek@MacBook-Pro-Bartosz.local>
2018-07-02 16:54:04 +0200

    Add

--- sandbox/src/test/java/ru/stqa/oft/sandbox/PointTest.java
+++ sandbox/src/test/java/ru/stqa/oft/sandbox/PointTest.java
@@ -17,4 +17,15 @@ public class PointTest {
 
     Assert.assertEquals(p.area(p1,p2), 3.1622776601683795);
   }
+  @Test
+  public  void testArea1(){
+
+    Point p1 = new Point(3, 3);
+    Point p2 = new Point(4, 4);
+
+
+    Point p = new Point(p1.x, p1.y);
+
+    Assert.assertEquals(p.area(p1,p2), 1.4142135623730951);
+  }
 }
