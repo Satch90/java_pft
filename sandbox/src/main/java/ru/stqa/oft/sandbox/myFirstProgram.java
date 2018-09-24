dac05d9668c203a9c4ba93334cc0357833fdcb93
Satch90 <40470572+Satch90@users.noreply.github.com>
2018-09-24 21:46:55 +0200

    Added fix to task 1

--- sandbox/src/main/java/ru/stqa/oft/sandbox/myFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/oft/sandbox/myFirstProgram.java
@@ -1,81 +1,16 @@
 package ru.stqa.oft.sandbox;
 
-import com.sun.xml.internal.bind.v2.util.EditDistance;
-import sun.awt.geom.AreaOp;
-import javax.swing.plaf.synth.SynthPopupMenuUI;
-
-import static java.lang.Math.pow;
-
 public class myFirstProgram {
 
   public static void main(String[] args) {
-    // Prints "Hello, World" to the terminal window.
-    //      hello("world");
-    //      hello("user");
-    //      hello("Bartosz");
 
-    //      double l = 5;
-    //      System.out.println("pole kwadratu o boku "+ l +" = " + area(l));
+    Point p1;
+    Point p2;
+    p1 = new Point(1.0, 1.0);
+    p2 = new Point(5.0, 1.0);
+    double distance = p1.distance(p2);
 
-    //      double a = 4;
-    //      double b = 6;
-    //      System.out.println("pole prostokatu o boku "+ a + " i " + b +" = " + area(a, b));
-
-
-    // System.out.println("Distance between points: +" + distance(p1, p2));
-
-    //double distance = distance(p1, p2);
-
-    //boolean weryfikacja = verify(p1, p2, distance);
-    // System.out.println("Verification status (true / false):" + weryfikacja);
-
-
-    Point p1 = new Point(1, 1);
-    Point p2 = new Point(6, 4);
-
-    String nowy = (asdf, asdf);
-
-
-
-    // System.out.println(distance);
-
-
-    System.out.println("Distance between points:" + p1.area(p1, p2));
-    System.out.println("Verification status (true / false):" + area(p1, p2));
-
-
-  }
-  // public static void hello(String somebody)  {
-  //     System.out.println("Hello, " + somebody +"!");
-  // }
-
-  // public static double area(double len){
-  //    return len * len;
-  // }
-
-  //public static double area(double a, double b){
-  //return a * b;
-  //}
-
-  public static boolean area(Point p1, Point p2) {
-    double dx;
-    double dy;
-    dx = p2.x - p1.x;
-    dy = p2.y - p1.y;
-
-    double wynik;
-    wynik = pow(dx, 2) + pow(dy, 2);
-
-
-    double distance = p1.area(p1, p2);
-
-    if ((int) wynik == (int) pow(distance, 2)) {
-      return true;
-
-    } else {
-      return false;
-    }
-
+    System.out.println("the result is " + distance);
 
   }
 }
--- sandbox/src/test/java/ru/stqa/oft/sandbox/PointTest.java
+++ sandbox/src/test/java/ru/stqa/oft/sandbox/PointTest.java
@@ -4,39 +4,10 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class PointTest {
-
-
   @Test
-  public  void testArea(){
-
-    Point p1 = new Point(1, 1);
-    Point p2 = new Point(2, 4);
-
-
-    Point p = new Point(p1.x, p1.y);
-
-    Assert.assertEquals(p.area(p1,p2), 3.1622776601683795);
-  }
-  @Test
-  public  void testArea1(){
-
-    Point p1 = new Point(3, 3);
-    Point p2 = new Point(4, 4);
-
-
-    Point p = new Point(p1.x, p1.y);
-
-    Assert.assertEquals(p.area(p1,p2), 1.4142135623730951);
-  }
-  @Test
-  public  void testArea2(){
-
-    Point p1 = new Point(6, 6);
-    Point p2 = new Point(9, 9);
-
-
-    Point p = new Point(p1.x, p1.y);
-
-    Assert.assertEquals(p.area(p1,p2), 4.242640687119285);
+  public void testArea() {
+    Point p1 = new Point(1.0, 1.0);
+    Point p2 = new Point(3.0, 1.0);
+    Assert.assertEquals( p1.distance(p2), 2.0);
   }
 }
