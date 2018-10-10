8af2cc415ba37d4cb31cbd39e68dbda5dfdfd6d4
Bartosz Kasperowicz <Bartek@MacBook-Pro-Bartosz.local>
2018-10-10 20:38:14 +0200

    added lambda

--- addressbook-web-test/src/test/java/ru/stqa/oft/addressbook/model/GroupData.java
+++ addressbook-web-test/src/test/java/ru/stqa/oft/addressbook/model/GroupData.java
@@ -3,14 +3,31 @@ package ru.stqa.oft.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
+  private int id;
   private final String name;
   private final String header;
   private final String footer;
 
+
   public GroupData(String name, String header, String footer) {
+    this.id = 0;
     this.name = name;
     this.header = header;
     this.footer = footer;
+  }
+  public GroupData(int id,String name, String header, String footer) {
+    this.id = id;
+    this.name = name;
+    this.header = header;
+    this.footer = footer;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
   }
 
   public String getName() { return name; }
@@ -18,22 +35,9 @@ public class GroupData {
   @Override
   public String toString() {
     return "GroupData{" +
-            "name='" + name + '\'' +
+            "id='" + id + '\'' +
+            ", name='" + name + '\'' +
             '}';
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    GroupData groupData = (GroupData) o;
-    return Objects.equals(name, groupData.name);
-  }
-
-  @Override
-  public int hashCode() {
-
-    return Objects.hash(name);
   }
 
   public String getHeader() {
@@ -43,4 +47,19 @@ public class GroupData {
   public String getFooter() {
     return footer;
   }
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData groupData = (GroupData) o;
+    return id == groupData.id &&
+            Objects.equals(name, groupData.name);
+  }
+
+  @Override
+  public int hashCode() {
+
+    return Objects.hash(id, name);
+  }
+
 }
--- addressbook-web-test/src/test/java/ru/stqa/oft/addressbook/tests/GroupModificationTest.java
+++ addressbook-web-test/src/test/java/ru/stqa/oft/addressbook/tests/GroupModificationTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.oft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTest extends TestBase {
@@ -21,13 +22,16 @@ public class GroupModificationTest extends TestBase {
 
     app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+    GroupData group = new GroupData(before.get(before.size() - 1).getId(),"test1", "test2", "test3");
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
-
     Assert.assertEquals(after.size(), before.size());
 
+    before.remove(before.size() - 1);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
     }
   }
